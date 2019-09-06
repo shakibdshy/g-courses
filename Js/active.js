328c0358dec6655d6600aa59eceece5b053b9112
shakibdshy <shakibdshy@gmail.com>
2019-09-07 01:04:21 +0600

    Owl-nav extend and brand area styled

--- Js/active.js
+++ Js/active.js
@@ -87,4 +87,22 @@ $('.active-testimonial').owlCarousel({
             }
       }
 })
+// Brand Slider Active
+$('.brand-active').owlCarousel({
+      loop  :true,
+      margin: 30,
+      nav   :true,
+      nevText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+      responsive:{
+            0:{
+                  items:2,
+            },
+            600:{
+                  items:4,
+            },
+            1000:{
+                  items:6
+            }
+      }
+})
 
